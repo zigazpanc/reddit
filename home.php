0b0194ebb496aa1391c61ef439e1a9416e45fedd
zigazpanc <zigazpanc@users.noreply.github.com>
2019-09-18 18:56:59 +0200

    Jow
    ddd

---
+++ home.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Reddit page</title>
+	<link href='' rel='stylesheet'>
+	<link rel='stylesheet' type='text/css' href='style.css'>
+	<script src="https://kit.fontawesome.com/67753fd32b.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div class="header">
+		<div class="inerheader">
+			<h1><img src="slike/redditlogo.png" height="50px" width="auto"></h1>
+		</div>
+		
+		<ul class="nav">
+			<a><li><div class="search">
+    
+
+			<a><li><button type="submit"><i class="fas fa-fire-alt"></i></button></li></a>
+				<a><li><button type="submit"><i class="fas fa-globe-europe"></i></button></li></a>
+			<a><li><button type="submit"><i class="fas fa-comments"></i></button></li></a>
+			<a><li><button type="submit"><i class="fas fa-envelope"></i></button></li></a>
+			<a><li><button type="submit"><i class="fas fa-file"></i></button></li></a>
+		</ul>
+	</div>
+	
+	<div class="posts">
+		<a>Hi</a>
+	</div>
+	
+</body>
+</html>
