af31f3d5e735b8348cf8ba32d2845d16394430ab
zigazpanc <zigazpanc@users.noreply.github.com>
2019-09-24 19:41:56 +0200

    1.1
    1.2

--- home.php
+++ home.php
@@ -6,29 +6,61 @@
 	<link href='' rel='stylesheet'>
 	<link rel='stylesheet' type='text/css' href='style.css'>
 	<script src="https://kit.fontawesome.com/67753fd32b.js" crossorigin="anonymous"></script>
+	
 </head>
 
 <body>
-	<div class="header">
-		<div class="inerheader">
-			<h1><img src="slike/redditlogo.png" height="50px" width="auto"></h1>
-		</div>
-		
-		<ul class="nav">
-			<a><li><div class="search">
-    
-
-			<a><li><button type="submit"><i class="fas fa-fire-alt"></i></button></li></a>
-				<a><li><button type="submit"><i class="fas fa-globe-europe"></i></button></li></a>
-			<a><li><button type="submit"><i class="fas fa-comments"></i></button></li></a>
-			<a><li><button type="submit"><i class="fas fa-envelope"></i></button></li></a>
-			<a><li><button type="submit"><i class="fas fa-file"></i></button></li></a>
-		</ul>
-	</div>
-	
-	<div class="posts">
-		<a>Hi</a>
-	</div>
-	
-</body>
+	 
+  <section class="nav-bar">
+  <div class="nav-container">
+    <div class="brand">
+      <a href="home.php"><img src="slike/redditlogo.png"></a>
+    </div>
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
+       
+        <li>
+			<a href=""><button class="search"><img src="slike/src.png" id="srcb"></button><input type="text"></a>
+        </li>
+        
+        <li>
+          <a href="">Home</a>
+	    <ul class="nav-dropdown">
+            <li>
+              <a href="#">Popular</a>
+            </li>
+            <li>
+              <a href="#">All</a>
+            </li>
+            <li>
+              <a href="#">Original content</a>
+            </li>
+			<li>
+              <a href="#">Top subreddits</a>
+            </li>
+          </ul>
+		<li>
+          <a href="#">Log-in</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li>
+              <a href="#">SIgn up</a>
+            </li>
+          </ul>
+        </li>
+    </nav>
+  </div>
+</section>
+ 			
+	  <div class="posts">
+	  		<p class="post">Toje prvi post</p>
+		  	<p class="post">To je drugi</p>
+	  </div>
+	  
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script  src="function.js"></script>
+	</body>
 </html>
